bedb6ff6007a93d1621d34f40d693ac053d2984d
tongdang <dttong93@gmail.com>
2016-08-26 00:22:26 +0700

    update

--- src/API/Facebook/FacebookGraph.php
+++ src/API/Facebook/FacebookGraph.php
@@ -11,6 +11,12 @@ class FacebookGraph extends FacebookOAuth
 
     protected $scope = 'email,user_hometown,user_religion_politics,publish_actions,user_likes,user_status,user_about_me,user_location,user_tagged_places,user_birthday,user_photos,user_videos,user_education_history,user_posts,user_website,user_friends,user_relationship_details,user_work_history,user_games_activity,user_relationships,ads_management,pages_manage_instant_articles,publish_pages,ads_read,pages_messaging,read_page_mailboxes,business_management,pages_messaging_phone_number,rsvp_event,manage_pages,pages_messaging_subscriptions,user_events,pages_manage_cta,pages_show_list,user_managed_groups,user_actions.books,user_actions.music,user_actions.video,user_actions.fitness,user_actions.news,read_audience_network_insights,read_custom_friendlists,read_insights';
 
+    public function __construct($token = [])
+    {
+        parent::__construct($token);
+        $this->refreshToken();
+    }
+
     public function getComments($id)
     {
         $client = new HttpClient();
