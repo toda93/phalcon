efaa5dceff4dfaca18eec4109a2aaccaa5c9b719
tongdang <dttong93@gmail.com>
2016-12-14 23:14:04 +0700

    update

--- src/API/Google/GoogleDrive.php
+++ src/API/Google/GoogleDrive.php
@@ -6,17 +6,33 @@ use Toda\Client\HttpClient;
 
 class GoogleDrive extends GoogleOAuth
 {
-    public function uploadImage($file)
+    public function uploadImage($image)
     {
+
+        $file_type = mime_content_type($image);
+        $delimiter = '---------------------------' . uniqid();
+
+        $data = '--' . $delimiter . "\r\n";
+        $data .= 'Content-Type: ' . $file_type . "\r\n\r\n";
+
+        $data .= json_encode([
+                "title" => basename($image),
+                "mimeType" => $file_type,
+            ]) . "\r\n";
+
+        $data = '--' . $delimiter . "\r\n";
+        $data .= 'Content-Type: ' . $file_type . "\r\n\r\n";
+
+        $data .= file_get_contents($image) . "\r\n";
+        $data .= '--' . $delimiter . "--\r\n";
+
 
         $client = new HttpClient();
 
-        $data = file_get_contents($file);
-
         $res = $client->init()
             ->addHeader("Authorization: Bearer " . $this->token['access_token'])
-            ->addHeader("Content-Type: " . mime_content_type($file))
-            ->addHeader("Content-Length" . strlen($data))
+            ->addHeader("Content-Type: " . 'multipart/form-data')
+            ->addHeader("Content-Type: multipart/form-data; boundary=$delimiter")
             ->post('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', $data, false);
 
         $res = json_decode($res, true);
