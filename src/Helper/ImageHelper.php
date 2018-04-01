3ca021400672237fc7e727e7f3f9760adeca87ce
tongdang <dttong93@gmail.com>
2018-04-02 00:05:04 +0700

    update

--- src/Helper/ImageHelper.php
+++ src/Helper/ImageHelper.php
@@ -1,7 +1,16 @@
 <?php
 namespace Toda\Helper;
 
+use Intervention\Image\ImageManagerStatic as Image;
+
 
 class ImageHelper
 {
+    public static function make($img)
+    {
+        Image::configure(array('driver' => 'imagick'));
+
+        return Image::make($img);
+
+    }
 }
